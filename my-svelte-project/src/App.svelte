0ecc0d22498ed13a62e262d1e34f7098921dc571
Morgan <121578632+morganharris39@users.noreply.github.com>
2024-09-14 19:09:27 -0600

    Uploaded svelte component

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import MyButton from './Button.svelte'
+</script>
+
+<svelte:head>
+<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+</svelte:head>
+
+<MyButton name="Button" type="default"/>
+
---
+++ my-svelte-project/src/Button.svelte
@@ -0,0 +1,22 @@
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+
+<script>
+	export let type = "default"
+	export let name = "Button"
+</script>
+
+{#if type=="default"}
+<button class="bg-blue-600 text-white px-40 py-3 m-3 rounded-full font-semibold hover:bg-blue-700">{name}</button>
+{/if}
+
+{#if type=="active"}
+<button class="bg-blue-700 text-white px-40 py-3 m-3 rounded-full font-semibold">{name}</button>
+{/if}
+
+{#if type=="disabled"}
+<button class="bg-gray-300 text-gray-500 px-40 py-3 m-3 rounded-full font-semibold">{name}</button>
+{/if}
